85b6d9b9e848f50f7af5a49a8da893c64392591b
minamtt <sytian_01@163.com>
2022-04-21 19:51:22 +0800

    f

--- javascript/categories.js
+++ javascript/categories.js
@@ -1,6 +1,46 @@
-var vm0 = new Vue({
+var vm = new Vue({
     el:"#main_container",
     data:{
+        cates:[
+            {
+                categoryID:1,
+                categoryName:"Education",
+                children:[
+                    {
+                        categoryID:101,
+                        categoryName:"Family"
+                    },
+                    {
+                        categoryID:102,
+                        categoryName:"Children"
+                    },
+                    {
+                        categoryID:103,
+                        categoryName:"Primary School"
+                    }
+                ]
+            },
+            {
+                categoryID:2,
+                categoryName:"Novel",
+                children:[
+                    {
+                        categoryID:201,
+                        categoryName:"Western"
+                    },
+                    {
+                        categoryID:202,
+                        categoryName:"Chinese"
+                    },
+                    {
+                        categoryID:203,
+                        categoryName:"Arabric"
+                    }
+                ]
+            }
+        ],
+        cate1_pointer:0,
+        cate2_pointer:0,
         currentPage:1,
         pages_total:4,
         books:[
@@ -30,7 +70,46 @@ var vm0 = new Vue({
     methods:{
         getBookDetail(number){
             location.href = `./bookdetail.html?isbn=${this.books[number].ISBN}`;
+        },
+        changeCate1(index){
+            this.cate1_pointer = index;
+            this.cate2_pointer = 0;
+        },
+        changeCate2(index){
+            this.cate2_pointer = index;
         }
+    },
+    computed:{
+        cates1(){
+            let arr = ["all"];
+            let maxlen = this.cates.length;
+            if (maxlen > 5){
+                maxlen = 5;
+            }
+            for (let i = 0;i < maxlen;i++){
+                arr.push(this.cates[i].categoryName);
+            }
+            return arr;
+        },
+        cates2(){
+            let arr = ["all"];
+            if (this.cate1_pointer === 0){
+                return arr;
+            }
+            else {
+                let maxlen = this.cates[this.cate1_pointer - 1].children.length;
+                if (maxlen > 5){
+                    maxlen = 5;
+                }
+                for (let i = 0;i < maxlen;i++){
+                    arr.push(this.cates[this.cate1_pointer - 1].children[i].categoryName);
+                }
+            }
+            return arr;
+        }
+    },
+    watch:{
+
     },
     mounted(){
         
